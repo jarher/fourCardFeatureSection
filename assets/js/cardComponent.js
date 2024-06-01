3388eb5ca3cc5a7625b334528dc2aee4a76fbfb9
jarher <jefferh33@gmail.com>
2024-05-31 19:39:29 -0500

    desktop css styles finished

--- assets/js/cardComponent.js
+++ assets/js/cardComponent.js
@@ -1,6 +1,12 @@
-export function Card({ cardTitle, cardDescription, imgUrl, imgAlt }) {
+export function Card({
+  cardTitle,
+  cardDescription,
+  imgUrl,
+  imgAlt,
+  cardClass,
+}) {
   return `
-    <div class="card">
+    <div class="card ${cardClass}">
         <h2>${cardTitle}</h2>
         <p>${cardDescription}</p>
         <img src="${imgUrl}" alt="${imgAlt}">
